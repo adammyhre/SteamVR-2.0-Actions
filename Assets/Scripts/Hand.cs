2944b6b765becc3676b8bf034d0a3de8cc4ae896
Adam Myhre <adam@undergalaxie.com>
2019-05-25 12:49:58 -0700

    Pickup and Drop Object

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,72 @@
+﻿#define DEBUG_Pickup
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour {
+
+	public SteamVR_Action_Boolean grabAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
+
+	private SteamVR_Behaviour_Pose trackedObj = null;
+	private FixedJoint joint = null;
+
+	private Interactable currentInteractable;
+	public List<Interactable> contactInteractables = new List<Interactable>(); 
+
+
+	private void Awake () {
+		trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
+		joint = GetComponent<FixedJoint>();
+	}
+	
+	private void Update () {
+
+		// Pickup and Drop Object code
+		// Trigger Down
+		if (grabAction.GetStateDown(trackedObj.inputSource)) {
+			#if DEBUG_Pickup
+			Debug.Log (trackedObj.inputSource + " trigger down.");
+			#endif
+			PickUp();
+		}
+
+		// Trigger Up
+		if (grabAction.GetStateUp(trackedObj.inputSource)) {
+			#if DEBUG_Pickup
+			Debug.Log (trackedObj.inputSource + " trigger up.");
+			#endif
+			Drop();
+		}
+
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if (!other.gameObject.CompareTag("Interactable"))
+			return;
+
+		contactInteractables.Add(other.gameObject.GetComponent<Interactable>());
+	}
+
+	private void OnTriggerExit (Collider other) {
+		if (!other.gameObject.CompareTag("Interactable"))
+			return;
+
+		contactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
+	}
+
+	public void PickUp () {
+
+	}
+
+	public void Drop () {
+
+	}
+
+	private Interactable GetNearestInteractable () {
+		return null;
+	}
+
+}
+
---
+++ Assets/Scripts/ViveInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour {
+
+	public SteamVR_Action_Boolean grabAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
+	public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport");
+	public SteamVR_Action_Single squeezeAction = SteamVR_Input.GetAction<SteamVR_Action_Single>("Squeeze");
+	public SteamVR_Action_Vector2 touchPadAction = SteamVR_Input.GetAction<SteamVR_Action_Vector2>("TouchpadTouch");
+
+	void Update () {
+
+		if (teleportAction.GetStateDown(SteamVR_Input_Sources.Any)){
+			print("Teleport down.");
+		}
+		if (grabAction.GetStateUp(SteamVR_Input_Sources.Any)){
+			print("Grab Pinch up.");
+		}
+
+		float triggerValue = squeezeAction.GetAxis (SteamVR_Input_Sources.Any);
+
+		if (triggerValue > 0.0f) {
+			print(triggerValue);
+		
+		}
+	
+		Vector2 touchPadValue = touchPadAction.GetAxis (SteamVR_Input_Sources.Any);
+
+		if (touchPadValue != Vector2.zero) {
+			print(touchPadValue);
+
+		}
+
+	}
+
+}
