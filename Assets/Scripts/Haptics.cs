e3f91453e6abc2e3cf9e5952a6a0c6d81d0da09d
Adam Myhre <adam@undergalaxie.com>
2019-05-31 16:20:43 -0700

    Haptics

---
+++ Assets/Scripts/Haptics.cs
@@ -0,0 +1,34 @@
+﻿#define DEBUG_Haptic
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Haptics : MonoBehaviour {
+
+	public SteamVR_Action_Vibration hapticAction = SteamVR_Input.GetAction<SteamVR_Action_Vibration>("Haptic");
+	public SteamVR_Action_Boolean trackpadAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport");
+
+
+	void Update () {
+		
+		if (trackpadAction.GetStateDown (SteamVR_Input_Sources.LeftHand)) {
+			Pulse (1, 150, 75, SteamVR_Input_Sources.LeftHand);
+		}
+		
+		if (trackpadAction.GetStateDown (SteamVR_Input_Sources.RightHand)) {
+			Pulse (1, 150, 75, SteamVR_Input_Sources.RightHand);
+		}
+
+	}
+
+
+	private void Pulse (float duration, float frequency, float amplitude, SteamVR_Input_Sources source) {
+		hapticAction.Execute (0, duration, frequency, amplitude, source);
+		#if DEBUG_Haptic
+		Debug.Log ("Pulse " + source.ToString());
+		#endif
+	} 
+
+}
